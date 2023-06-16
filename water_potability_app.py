7eb5200a16fe95864115650fa71acb3ae866a41e
Rohan-kolewad7 <132587323+Rohan-kolewad7@users.noreply.github.com>
2023-06-16 17:54:55 +0530

    Update water_potability_app.py

--- water_potability_app.py
+++ water_potability_app.py
@@ -17,6 +17,44 @@ st.set_page_config(layout='wide')
 with open('random_forest_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
+
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+video_html = """
+		<style>
+
+		#myVideo {
+		  position: fixed;
+		  right: 0;
+		  bottom: 0;
+		  min-width: 100%; 
+		  min-height: 100%;
+		}
+
+		.content {
+		  position: fixed;
+		  bottom: 0;
+		  background: rgba(0, 0, 0, 0.5);
+		  color: #f1f1f1;
+		  width: 100%;
+		  padding: 20px;
+		}
+
+		</style>	
+		<video autoplay muted loop id="myVideo">
+		  <source src="https://www.pexels.com/video/close-up-shot-of-water-droplets-falling-onto-a-calm-water-9667531/")>
+		  Your browser does not support HTML5 video.
+		</video>
+        """
+
+st.markdown(video_html, unsafe_allow_html=True)
+st.title('Video page')
+
+
+
+
 def add_bg_from_local(image_file, opacity):
     with open(image_file, "rb") as file:
         encoded_string = base64.b64encode(file.read()).decode()
