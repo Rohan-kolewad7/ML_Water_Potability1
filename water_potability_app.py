efc5dfe6a4e29c148f3bf37841f24a42f672841e
Rohan-kolewad7 <132587323+Rohan-kolewad7@users.noreply.github.com>
2023-06-16 18:02:24 +0530

    Update water_potability_app.py

--- water_potability_app.py
+++ water_potability_app.py
@@ -19,40 +19,11 @@ with open('random_forest_model.pkl', 'rb') as file:
 
 #adding video
 
-video_html = """
-		<style>
-
-		#myVideo {
-		  position: fixed;
-		  right: 0;
-		  bottom: 0;
-		  min-width: 100%; 
-		  min-height: 100%;
-		}
-
-		.content {
-		  position: fixed;
-		  bottom: 0;
-		  background: rgba(0, 0, 0, 0.5);
-		  color: #f1f1f1;
-		  width: 100%;
-		  padding: 20px;
-		}
-
-		</style>	
-		<video autoplay muted loop id="myVideo">
-		  <source src="https://www.pexels.com/video/close-up-shot-of-water-droplets-falling-onto-a-calm-water-9667531/")>
-		  Your browser does not support HTML5 video.
-		</video>
-        """
-
-st.markdown(video_html, unsafe_allow_html=True)
-st.title('Video page')
 
 
 
 
-''' def add_bg_from_local(image_file, opacity):
+def add_bg_from_local(image_file, opacity):
     with open(image_file, "rb") as file:
         encoded_string = base64.b64encode(file.read()).decode()
 
@@ -72,7 +43,7 @@ st.title('Video page')
     )
 
 # Example usage with opacity of 0.9
-add_bg_from_local('img4.jpg', opacity=0.9) '''
+add_bg_from_local('img4.jpg', opacity=0.9) 
 
 
 # Define function to preprocess user input
